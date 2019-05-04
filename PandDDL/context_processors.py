eda6e069d8705184305258a68df803a6db14297e
steve-31 <stephen.lynn318@gmail.com>
2019-05-04 19:34:35 +0100

    styling changes

--- PandDDL/context_processors.py
+++ PandDDL/context_processors.py
@@ -9,8 +9,14 @@ def header_processor(request):
         team=""
         
     active_leagues = LeagueGrp.objects.filter(active=True).order_by('-gender')
-    active_mens = LeagueGrp.objects.get(active=True, gender="Men's")
-    active_ladies = LeagueGrp.objects.get(active=True, gender="Ladies")
+    try:
+        active_mens = LeagueGrp.objects.get(active=True, gender="Men's")
+    except LeagueGrp.DoesNotExist:
+        active_mens = None
+    try: 
+        active_ladies = LeagueGrp.objects.get(active=True, gender="Ladies")
+    except LeagueGrp.DoesNotExist:
+        active_ladies = None
     active_divisions = Division.objects.filter(leaguegrp__in=active_leagues)
     
     return { "logged_in_team": team, "active_divisions": active_divisions, "active_leagues": active_leagues, "active_mens": active_mens, "active_ladies": active_ladies }
